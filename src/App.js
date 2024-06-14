dcb56402ef47385c1618e5851314cfbad78fa379
BurakAliksei <burakalexey1@gmail.com>
2024-06-14 15:12:11 +0300

    There is a mistake but i cannot see it

--- src/App.js
+++ src/App.js
@@ -1,87 +1,52 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-      <input
-      className='search monsters'
-      type='search'
-      placeholder='search monsters'
-      onChange={(event) => {
-        console.log(event.target.value);
-        const searchString = event.target.value.toLocaleLowerCase();
-        const filteredMonsters = this.state.monsters.filter((monster) => {
-          return monster.name.toLocaleLowerCase().includes(searchString);
-        });
-      this.setState(() => {
-        return {monstars:filteredMonsters};
-      })
->>>>>>> Stashed changes
-    </div>
-  );
-}
-=======
-import { useState, useEffect } from 'react';
-
-import CardList from './components/card-list/card-list.component';
-import SearchBox from './components/search-box/search-box.component';
-import './App.css';
-
-const App = () => {
-  const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilterMonsters] = useState(monsters);
-
-  useEffect(() => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
-  }, []);
-
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    setFilterMonsters(newFilteredMonsters);
-  }, [monsters, searchField]);
-
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
-
-  return (
-    <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
-
-      <SearchBox
-        className='monsters-search-box'
-        onChangeHandler={onSearchChange}
+    .then((response) => response.json())
+    .then((users)=> this.setState(() => {
+      return {monsters: users};
+    }, 
+  () => {
+    console.log(this.state);
+  }
+)
+    );
+  } 
+  render() {
+    console.log('render');
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    //There is a mistake but i cannot see it
+    const filteredMonsters = monster.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
+    return (
+      <div className='App'>
+        <input
+         className='search-box'
+        type='search'
         placeholder='search monsters'
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
-};
->>>>>>> Stashed changes
-
+        onchange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+              </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+    
+    
 export default App;
