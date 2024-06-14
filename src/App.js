c05226436b5099656c510bf862175bfbb68db36a
BurakAliksei <burakalexey1@gmail.com>
2024-06-14 14:55:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +20,68 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <input
+      className='search monsters'
+      type='search'
+      placeholder='search monsters'
+      onChange={(event) => {
+        console.log(event.target.value);
+        const searchString = event.target.value.toLocaleLowerCase();
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+          return monster.name.toLocaleLowerCase().includes(searchString);
+        });
+      this.setState(() => {
+        return {monstars:filteredMonsters};
+      })
+>>>>>>> Stashed changes
     </div>
   );
 }
+=======
+import { useState, useEffect } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+import './App.css';
+
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+>>>>>>> Stashed changes
 
 export default App;
